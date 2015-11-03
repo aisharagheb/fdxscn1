1b32d85dba83ad3fb40bd7a7d978e49b01ad06a0
Kyle Olson <kolson@four51.com>
2015-11-03 17:01:11 -0600

    Change Password

--- src/app/account/account.js
+++ src/app/account/account.js
@@ -2,6 +2,8 @@ angular.module( 'orderCloud' )
 
 	.config( AccountConfig )
 	.controller( 'AccountCtrl', AccountController )
+	.factory( 'ChangePasswordService', ChangePasswordService )
+	.controller( 'ChangePasswordCtrl', ChangePasswordController )
 
 ;
 
@@ -14,6 +16,17 @@ function AccountConfig( $stateProvider ) {
 			controllerAs: 'account',
 			resolve: {
 				Profile: function(Me) {
+					return Me.Get();
+				}
+			}
+		})
+		.state( 'base.changePassword', {
+			url: '/account/changepassword',
+			templateUrl: 'account/templates/changePassword.tpl.html',
+			controller: 'ChangePasswordCtrl',
+			controllerAs: 'changePassword',
+			resolve: {
+				CurrentUser: function(Me) {
 					return Me.Get();
 				}
 			}
@@ -40,5 +53,55 @@ function AccountController( $exceptionHandler, toastr, Profile, AdminUsers ) {
 	vm.resetForm = function(form) {
 		vm.profile = currentProfile;
 		form.$setPristine(true);
+	};
+}
+
+function ChangePasswordService( $q, Credentials, AdminUsers ) {
+	var service = {
+		ChangePassword: _changePassword
+	};
+
+	function _changePassword(currentUser) {
+		var deferred = $q.defer();
+
+		var checkPasswordCredentials = {
+			Username: currentUser.Username,
+			Password: currentUser.CurrentPassword
+		};
+
+		function changePassword() {
+			currentUser.Password = currentUser.NewPassword;
+			AdminUsers.Update(currentUser.ID, currentUser)
+				.then(function() {
+					deferred.resolve();
+				});
+		}
+
+		Credentials.Get(checkPasswordCredentials).then(
+			function() {
+				changePassword();
+			}).catch(function( ex ) {
+				deferred.reject(ex);
+			});
+
+		return deferred.promise;
+	}
+
+	return service;
+}
+
+function ChangePasswordController( $state, $exceptionHandler, toastr, ChangePasswordService, CurrentUser ) {
+	var vm = this;
+	vm.currentUser = CurrentUser;
+
+	vm.changePassword = function() {
+		ChangePasswordService.ChangePassword(vm.currentUser)
+			.then(function() {
+				toastr.success('Password successfully changed', 'Success!');
+				$state.go('base.account');
+			})
+			.catch(function(ex) {
+				$exceptionHandler(ex)
+			});
 	};
 }
