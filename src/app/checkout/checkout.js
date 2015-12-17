ac7ddba51b330deb3d0b224ae61685328b7fb4bc
Robert <rwatt@four51.com>
2015-12-17 14:29:04 -0600

    Checkout Resolves DEVCTR-16099

--- src/app/checkout/checkout.js
+++ src/app/checkout/checkout.js
@@ -1,7 +1,7 @@
 angular.module('orderCloud')
 	.config(checkoutConfig)
 	.controller('CheckoutCtrl', CheckoutController)
-	.controller('OrderSummaryCtrl', OrderReviewController)
+	.controller('OrderReviewCtrl', OrderReviewController)
 	.controller('OrderConfirmationCtrl', OrderConfirmationController)
 ;
 
@@ -27,8 +27,24 @@ function checkoutConfig($stateProvider) {
 						});
 					return dfd.promise;
 				},
-				LineItemsList: function(CurrentOrder, LineItems) {
-					return LineItems.Get(CurrentOrder.ID);
+				LineItemsList: function($q, Underscore, Products, LineItems, CurrentOrder) {
+					var dfd = $q.defer();
+					LineItems.Get(CurrentOrder.ID)
+						.then(function(data) {
+							var productQueue =[];
+							var productIDs = Underscore.pluck(data.Items, 'ProductID');
+							angular.forEach(productIDs, function(id) {
+								productQueue.push(Products.Get(id));
+							});
+							$q.all(productQueue)
+								.then(function(results) {
+									angular.forEach(data.Items, function(li) {
+										li.Product = angular.copy(Underscore.where(results, {ID:li.ProductID})[0]);
+									});
+									dfd.resolve(data);
+								})
+						});
+					return dfd.promise;
 				}
 			}
 		})
@@ -56,8 +72,8 @@ function checkoutConfig($stateProvider) {
 
 function CheckoutController(CurrentOrder, LineItemsList) {
 	var vm = this;
+	vm.lineItems = LineItemsList;
 	vm.currentOrder = CurrentOrder;
-	vm.currentOrder.LineItems = LineItemsList;
 }
 
 function OrderReviewController() {
