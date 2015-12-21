1c7dc059d8890a9b19d01eb33f9659946285612c
Miranda Posthumus <mposthumus@four51.com>
2015-12-21 12:31:21 -0600

    DEVCTR-16203 updated specs unit tests to account for changes

--- src/app/specs/tests/specs.spec.js
+++ src/app/specs/tests/specs.spec.js
@@ -14,6 +14,7 @@ describe('Component: Specs', function() {
             Required: false,
             DefinesVariant: false,
             AllowOpenText: false,
+            DefaultOptionID: null,
             Options: [
                 {
                     ID: "TestSpecOpt123456789",
@@ -84,31 +85,58 @@ describe('Component: Specs', function() {
         }));
 
         describe('addSpecOpt', function() {
-            beforeEach(inject(function() {
+            beforeEach(inject(function(Specs) {
                 specEditCtrl.spec = spec;
-                specEditCtrl.SpecOptID = "TestSpecOpt987654321";
-                specEditCtrl.SpecOptValue = 1;
-                specEditCtrl.SpecOptMarkup = null;
-                specEditCtrl.SpecOptMarkupType = null;
-                specEditCtrl.SpecOptOpen = false;
-                specEditCtrl.SpecOptListOrder = 2;
-                specEditCtrl.SpecOptDefault = false;
+                specEditCtrl.specID = "TestSpec123456789";
+                specEditCtrl.Option = {
+                    ID: "TestSpecOpt123456789",
+                    Value: "test"
+                };
+                var defer = q.defer();
+                defer.resolve(spec);
+                spyOn(Specs, 'CreateOption').and.returnValue(defer.promise);
+                specEditCtrl.DefaultOptionID = true;
                 specEditCtrl.addSpecOpt();
             }));
-            it ('should push the option to the Spec Options array', inject(function() {
-                expect(spec.Options.length).toEqual(2);
+            it ('should call the Specs CreateOption method', inject(function(Specs) {
+                expect(Specs.CreateOption).toHaveBeenCalledWith(specEditCtrl.specID, specEditCtrl.Option);
+            }));
+            it ('should set specEditCtrl.spec.DefaultOption ID to specEditCtrl.Option.ID', inject(function() {
+                expect(spec.DefaultOptionID).toEqual(specEditCtrl.Option.ID);
+            }));
+            it ('should set specEditCtrl.Option to null', inject(function() {
+                scope.$digest();
+                expect(specEditCtrl.Option).toEqual(null);
             }));
         });
 
         describe('deleteSpecOpt', function() {
-            beforeEach(inject(function() {
+            beforeEach(inject(function(Specs) {
                 specEditCtrl.spec = spec;
-                spec.DefaultOptionID = null;
+                specEditCtrl.specID = "TestSpec123456789";
+                specEditCtrl.spec.DefaultOptionID = specEditCtrl.spec.Options[0].ID;
+                specEditCtrl.Options = [
+                    {
+                        ID: "TestSpecOpt1234567890",
+                        Value: "test"
+                    },
+                    {
+                        ID: "TestSpecOpt123456789123",
+                        Value: "test"
+                    }
+                ];
                 var index = 0;
+                spyOn(Specs, 'DeleteOption').and.returnValue(null);
                 specEditCtrl.deleteSpecOpt(index);
             }));
             it ('should splice the option from the Spec Options array', inject(function() {
-                expect(spec.Options.length).toEqual(0);
+                expect(specEditCtrl.Options.length).toEqual(1);
+            }));
+            it ('should call the Specs DeleteOption method', inject(function(Specs) {
+                expect(Specs.DeleteOption).toHaveBeenCalledWith(specEditCtrl.specID, specEditCtrl.spec.Options[0].ID);
+            }));
+            it ('should set DefaultOptionID to null because the deleted Option was set as Default', inject(function() {
+                expect(specEditCtrl.spec.DefaultOptionID).toEqual(null);
             }));
         });
 
@@ -158,41 +186,73 @@ describe('Component: Specs', function() {
         describe('addSpecOpt', function() {
             beforeEach(inject(function() {
                 specCreateCtrl.spec = spec;
-                specCreateCtrl.SpecOptID = "TestSpecOpt987654321";
-                specCreateCtrl.SpecOptValue = 1;
-                specCreateCtrl.SpecOptMarkup = null;
-                specCreateCtrl.SpecOptMarkupType = null;
-                specCreateCtrl.SpecOptOpen = false;
-                specCreateCtrl.SpecOptListOrder = 2;
-                specCreateCtrl.SpecOptDefault = false;
                 specCreateCtrl.addSpecOpt();
             }));
             it ('should push the option to the Spec Options array', inject(function() {
-                expect(spec.Options.length).toEqual(2);
+                expect(specCreateCtrl.Options.length).toEqual(1);
             }));
+            it ('should set specCreateCtrl.Option to null', inject(function() {
+                expect(specCreateCtrl.Option).toEqual(null);
+            }));
+            it ('should set specCreateCtrl.DefaultOptionID to null', inject(function() {
+                expect(specCreateCtrl.DefaultOptionID).toEqual(null);
+            }));
+
         });
         describe('deleteSpecOpt', function() {
             beforeEach(inject(function() {
                 specCreateCtrl.spec = spec;
-                spec.DefaultOptionID = null;
+                specCreateCtrl.specID = "TestSpec123456789";
+                specCreateCtrl.spec.DefaultOptionID = specCreateCtrl.spec.Options[0].ID;
+                specCreateCtrl.Options = [
+                    {
+                        ID: "TestSpecOpt123456789",
+                        Value: "test"
+                    },
+                    {
+                        ID: "TestSpecOpt123456789123",
+                        Value: "test"
+                    }
+                ];
                 var index = 0;
                 specCreateCtrl.deleteSpecOpt(index);
             }));
             it ('should splice the option from the Spec Options array', inject(function() {
-                expect(spec.Options.length).toEqual(0);
+                expect(specCreateCtrl.Options.length).toEqual(1);
+            }));
+            it ('should set DefaultOptionID to null because the deleted Option was set as Default', inject(function() {
+                expect(specCreateCtrl.spec.DefaultOptionID).toEqual(null);
             }));
         });
         describe('Submit', function() {
             beforeEach(inject(function(Specs) {
                 specCreateCtrl.spec = spec;
+                specCreateCtrl.specID = "TestSpec123456789";
+                specCreateCtrl.Options = [
+                    {
+                        ID: "TestSpecOpt123456789",
+                        Value: "test"
+                    },
+                    {
+                        ID: "TestSpecOpt123456789123",
+                        Value: "test"
+                    }
+                ];
                 var defer = q.defer();
                 defer.resolve(spec);
                 spyOn(Specs, 'Create').and.returnValue(defer.promise);
+                spyOn(Specs, 'CreateOption').and.returnValue(defer.promise);
                 specCreateCtrl.Submit();
                 scope.$digest();
             }));
             it ('should call the Specs Create method', inject(function(Specs) {
                 expect(Specs.Create).toHaveBeenCalledWith(spec);
+            }));
+            it ('should call the Specs CreateOption method', inject(function(Specs) {
+                expect(Specs.CreateOption).toHaveBeenCalledWith(specCreateCtrl.specID, specCreateCtrl.Options[0]);
+            }));
+            it ('should call the Specs CreateOption method', inject(function(Specs) {
+                expect(Specs.CreateOption).toHaveBeenCalledWith(specCreateCtrl.specID, specCreateCtrl.Options[1]);
             }));
             it ('should enter the specs state', inject(function($state) {
                 expect($state.go).toHaveBeenCalledWith('specs', {}, {reload:true});
